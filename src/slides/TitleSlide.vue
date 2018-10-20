a9e761977ed65a8c73d7ce5b856e51258eb60834
Björn Ganslandt <b.ganslandt@gmail.com>
2018-10-20 14:16:53 +0200

    Add interactive alignment

--- src/slides/TitleSlide.vue
+++ src/slides/TitleSlide.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="title-container">
+  <div class="title-container" :style="{'text-align': alignment, 'align-items': flexAlignment}">
     <div class="icons">
-      <span class="icons__item icons__item--active"></span>
-      <span class="icons__item"></span>
-      <span class="icons__item"></span>
-      <span class="icons__item"></span>
+      <input type="radio" v-model="alignment" value="left" id="left" name="align" class="visually-hidden icons__input" />
+      <label class="icons__item" for="left"></label>
+      <input type="radio" v-model="alignment" value="center" id="center" name="align" class="visually-hidden icons__input" />
+      <label class="icons__item" for="center"></label>
+      <input type="radio" v-model="alignment" value="right" id="right" name="align" class="visually-hidden icons__input" />
+      <label class="icons__item" for="right"></label>
+      <input type="radio" v-model="alignment" value="justify" id="justify" name="align" class="visually-hidden icons__input" />
+      <label class="icons__item" for="justify"></label>
     </div>
     <h1>Aligning Development & Design</h1>
     <div class="author">
@@ -15,7 +19,27 @@
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            alignment: "left",
+        };
+    },
+    computed: {
+        flexAlignment: function() {
+            switch (this.alignment) {
+                case "left":
+                    return "flex-start";
+                case "right":
+                    return "flex-end";
+                case "justify":
+                    return "stretch";
+                default:
+                    return this.alignment;
+            }
+        },
+    },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -38,8 +62,10 @@ export default {};
 .icons__item {
     font-family: "Material-Design-Iconic-Font";
     font-size: $space * 5;
+    cursor: pointer;
 
-    &--active {
+    .icons__input:checked + &,
+    &:hover {
         color: $blue;
     }
 }
