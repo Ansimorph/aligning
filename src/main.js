06aa7a401eb3ad65865dd19838a4e14005cbf287
Björn Ganslandt <b.ganslandt@gmail.com>
2018-10-29 09:43:36 +0100

    Remove highlight js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,6 @@ import App from "./App.vue";
 
 import Eagle from "eagle.js";
 
-// import hljs from "highlight.js";
-// import { Options } from "eagle.js";
-// Options.hljs = hljs;
-
 Vue.use(Eagle);
 
 Vue.config.productionTip = false;
